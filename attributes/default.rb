b5eca73629fdd976245d04bcdc379531c4622be1
Paul Boone <pboone@topspinmedia.com>
2013-05-09 18:51:46 -0700

    make ruby version used into an attribute to avoid ohai language problems

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,9 @@ default['passenger']['version']     = '3.0.19'
 default['passenger']['apache_mpm']  = nil
 default['passenger']['root_path']   = "#{languages['ruby']['gems_dir']}/gems/passenger-#{passenger['version']}"
 default['passenger']['module_path'] = "#{passenger['root_path']}/ext/apache2/mod_passenger.so"
+default['passenger']['ruby'] = node['languages']['ruby']['ruby_bin']
 default['passenger']['max_pool_size'] = 6
 default['passenger']['manage_module_conf'] = true
 default['passenger']['package']['name'] = nil
 default['passenger']['package']['version'] = node['passenger']['version']
+
